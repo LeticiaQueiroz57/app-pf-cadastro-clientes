639e504fca0be6286bb82baee9293849ab9216c6
João Basílio <146891284+joaobasilio77@users.noreply.github.com>
2024-11-11 16:39:57 -0300

    Update userModel.py
    Modelo mudado para parametros Optional

--- src/models/userModel.py
+++ src/models/userModel.py
@@ -1,31 +1,11 @@
 from pydantic import BaseModel, Field, field_validator, model_validator
 from email_validator import validate_email, EmailNotValidError
-
+from typing import Optional
 class userModel(BaseModel):
-    nome: str = Field(..., description="nome não pode ser nulo")
-    sobrenome: str = Field(..., description="sobrenome não pode ser nulo")
-    email: str = Field(..., description="email válido é necessário")
-    telefone: str = Field(..., description="telefone não pode ser nulo")
-    preco: float = Field(..., description="preço deve ser positivo")
+    nome: Optional[str] = Field(None, description="nome não pode ser nulo")
+    sobrenome: Optional[str] = Field(None, description="sobrenome não pode ser nulo")
+    email: Optional[str] = Field(None, description="email válido é necessário")
+    telefone: Optional[str] = Field(None, description="telefone não pode ser nulo")
+    
 
-    @model_validator(mode="before")
-    def verificar_email(cls, values):
-        email = values.get('email')
-        try:
-            validate_email(email)
-        except EmailNotValidError as e:
-            raise ValueError("O email contém caracteres inválidos")  
-        return values
-
-    @model_validator(mode="before")
-    def preco_nao_ser_null(cls, values):
-        if values.get('preco', 0) <= 0:
-            raise ValueError('O preço deve ser positivo.')
-        return values
-
-    @model_validator(mode="before")
-    def must_not_be_empty(cls, values):
-        for field in ['nome', 'sobrenome', 'telefone']:
-            if not values.get(field) or values.get(field).strip() == "":
-                raise ValueError(f'{field} não pode ser nulo')
-        return values
+    
